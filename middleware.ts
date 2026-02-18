4db10ce88b6e2442d7cd78ac3049b19e22b1eb35
Leinerwill25 <websitesdr5@gmail.com>
2026-02-18 15:03:55 -0400

    correnccion en microfono

--- middleware.ts
+++ middleware.ts
@@ -71,7 +71,7 @@ export async function middleware(request: NextRequest) {
 	response.headers.set('X-Frame-Options', 'DENY');
 	response.headers.set('X-Content-Type-Options', 'nosniff');
 	response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
-	response.headers.set('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
+	response.headers.set('Permissions-Policy', 'camera=(self), microphone=(self), geolocation=(self)');
 	response.headers.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
 
 	// Si es una petición de API y el origen es permitido, añadir cabeceras CORS
